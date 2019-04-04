afc555e359251d05bcba5897104837e81c54b9a5
igorkulichok@gmail.com <igorkulichok@gmail.com>
2019-04-04 15:59:14 +0300

    JS homework

---
+++ script.js
@@ -0,0 +1,44 @@
+//завдання 1
+
+var seconds = 60;
+var minutes = 60;
+var hour = 3;
+var secondsInHours = seconds * minutes * hour;
+
+console.log('in ' + hour + ' hours ' + secondsInHours + ' seconds ');
+
+//завдання 2
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+
+alert(num);
+
+//завдання 3
+
+var a = 10;
+var b = 2;
+var c = 5;
+var sum = a + b + c;
+
+console.log('3 завдання - ' + sum);
+
+//завдання 4
+
+var a = 17; 
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+
+console.log('4 завдання - ' + result);
+
+//завдання 5
+
+var name = 'Игорь';
+alert('Привет ' + name + '!');
